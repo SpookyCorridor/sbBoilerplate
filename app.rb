8587b2ec1969db8eb2c0c6134e1d93241d0c6852
Dylan <invision33@gmail.com>
2015-08-20 15:22:26 -0500

    more exercises

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require 'bundler'
 Bundler.require
 
+ActiveRecord::Base.establish_connection(
+	:adapter => 'postgresql',
+	:database => 'instafake'
+)
+
 get '/' do 
 	erb :index 
 end 
@@ -19,4 +24,65 @@ end
 
 get '/4' do 
 	erb :four 
+end 
+
+get '/5' do 
+	erb :five
+end 
+
+get '/6' do 
+	erb :six 
+end 
+
+get '/7' do 
+	erb :seven 
+end 
+
+get '/8' do 
+	erb :eight
+end 
+
+get '/what' do 
+	erb :what 
+end 
+
+### restful api 
+
+get '/api/instafake' do 
+	InstagramModel.all.to_json 
+end 
+
+get '/api/instafake/:id' do 
+	InstagramModel.find(params[:id]).to_json
+end 
+
+post '/api/instafake' do 
+	InstagramModel.create(params).to_json
+end 
+
+put '/api/instafake/:id' do 
+	@id = params[:id]
+	@insta = InstagramModel.find(@id)
+	@insta.username = params[:username]
+	@isnta.post = params[:post]
+	@insta.description = params[:description]
+	@insta.hashtags = params[:hashtags]
+	@insta.save 
+	@insta.to_json
+end 
+
+patch '/api/instafake/:id' do 
+	@id = params[:id]
+	@insta = InstagramModel.find(@id)
+	@insta.username = params[:username]
+	@isnta.post = params[:post]
+	@insta.description = params[:description]
+	@insta.hashtags = params[:hashtags]
+	@insta.save 
+	@insta.to_json
+end 
+
+
+delete '/api/instafake/:id' do 
+	InstagramModel.destroy(params[:id]).to_json
 end 
---
+++ migrations.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE instafake;
+
+\c instafake;
+
+CREATE TABLE Instagram (id SERIAL PRIMARY KEY, username VARCHAR(255),
+post VARCHAR(255), description VARCHAR(255), hashtags VARCHAR(255)); 
